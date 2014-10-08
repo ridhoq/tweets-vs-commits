0beef0e6b914d3a03bbdfb85369c4351af70f6f7
Ridwan Hoq <ridwanhoq@gmail.com>
2014-10-08 02:23:44 -0500

    fix gulp serve issue when refreshing/saving files

--- gulp/build.js
+++ gulp/build.js
@@ -13,7 +13,11 @@ function handleError(err) {
 
 gulp.task('styles', function () {
   return gulp.src('app/styles/*.scss')
-    .pipe($.rubySass({style: 'expanded'}))
+    .pipe($.rubySass({
+      style: 'expanded',
+      sourcemap: true,
+      sourcemapPath: '/styles'
+    }))
     .on('error', handleError)
     .pipe($.autoprefixer('last 1 version'))
     .pipe(gulp.dest('.tmp/styles'))
