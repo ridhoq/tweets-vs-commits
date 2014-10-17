b7c8f62db17244c5ee4a97cdfaff46f515f6c18a
Ridwan Hoq <ridwanhoq@gmail.com>
2014-10-17 07:39:44 -0500

    refactor MainCtrl Twitter method names and use ng-show/ng-hide instead of jQuery

--- app/scripts/main/main-ctrl.js
+++ app/scripts/main/main-ctrl.js
@@ -4,41 +4,34 @@ angular.module('tweetsVsCommits')
   .controller('MainCtrl', function ($scope, $q, twitterService) {
 
   $scope.tweets;
-  twitterService.initialize();
+  $scope.twitterService = twitterService;
+  $scope.twitterService.initialize();
 
   //using the OAuth authorization result get the latest 20 tweets from twitter for the user
   $scope.refreshTimeline = function () {
-    twitterService.getLatestTweets().then(function (data) {
+    $scope.twitterService.getLatestTweets().then(function (data) {
       $scope.tweets = data;
     });
   };
 
   //when the user clicks the connect twitter button, the popup authorization window opens
-  $scope.connectButton = function () {
-    twitterService.connectTwitter().then(function () {
-      if (twitterService.isReady()) {
+  $scope.connectTwitterButton = function () {
+    $scope.twitterService.connectTwitter().then(function () {
+      if ($scope.twitterService.isReady()) {
         //if the authorization is successful, hide the connect button and display the tweets
-        $('#connectButton').fadeOut(function () {
-          $('#getTimelineButton, #signOut').fadeIn();
-          $scope.refreshTimeline();
-        });
+        $scope.refreshTimeline();
       }
     });
   };
 
   //sign out clears the OAuth cache, the user will have to reauthenticate when returning
-  $scope.signOut = function () {
-    twitterService.clearCache();
+  $scope.signOutTwitter = function () {
+    $scope.twitterService.clearCache();
     $scope.tweets.length = 0;
-    $('#getTimelineButton, #signOut').fadeOut(function () {
-      $('#connectButton').fadeIn();
-    });
   };
 
   //if the user is a returning user, hide the sign in button and display the tweets
-  if (twitterService.isReady()) {
-    $('#connectButton').hide();
-    $('#getTimelineButton, #signOut').show();
+  if ($scope.twitterService.isReady()) {
     $scope.refreshTimeline();
   }
 });
