f67820329a50e59777d0cbf08191ed27586dce42
Ridwan Hoq <ridwanhoq@gmail.com>
2014-10-17 16:25:57 -0500

    gulp deploy is kinda working? have to build manually

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -3,7 +3,13 @@
 var gulp = require('gulp');
 var deploy = require('gulp-gh-pages');
 
-gulp.task('deploy', ['build'], function () {
+var options = {
+  'remoteUrl': 'https://github.com/ridhoq/tweets-vs-commits.git',
+  'origin': 'origin',
+  'branch': 'gh-pages'
+};
+
+gulp.task('deploy', function () {
   return gulp.src('dist/**/*')
     .pipe(deploy());
 });
