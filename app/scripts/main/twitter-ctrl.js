50d62bab1c3e9f9c351b383ffeb6ade09801cce2
Ridwan Hoq <ridwanhoq@gmail.com>
2014-10-17 05:09:05 -0500

    add initial TwitterCtrl implementation

---
+++ app/scripts/main/twitter-ctrl.js
@@ -0,0 +1,41 @@
+'use strict';
+
+angular.module('tweetsVsCommits')
+  .controller('TwitterCtrl', function ($scope, $q, twitterService) {
+
+  //using the OAuth authorization result get the latest 20 tweets from twitter for the user
+  $scope.refreshTimeline = function () {
+    twitterService.getLatestTweets().then(function (data) {
+      $scope.tweets = data;
+    });
+  };
+
+  //when the user clicks the connect twitter button, the popup authorization window opens
+  $scope.connectButton = function () {
+    twitterService.connectTwitter().then(function () {
+      if (twitterService.isReady()) {
+        //if the authorization is successful, hide the connect button and display the tweets
+        $('#connectButton').fadeOut(function () {
+          $('#getTimelineButton, #signOut').fadeIn();
+          $scope.refreshTimeline();
+        });
+      }
+    });
+  };
+
+  //sign out clears the OAuth cache, the user will have to reauthenticate when returning
+  $scope.signOut = function () {
+    twitterService.clearCache();
+    $scope.tweets.length = 0;
+    $('#getTimelineButton, #signOut').fadeOut(function () {
+      $('#connectButton').fadeIn();
+    });
+  };
+
+  //if the user is a returning user, hide the sign in button and display the tweets
+  if (twitterService.isReady()) {
+    $('#connectButton').hide();
+    $('#getTimelineButton, #signOut').show();
+    $scope.refreshTimeline();
+  }
+});
